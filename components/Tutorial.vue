f132e98af12cf8c0e6b336a944e162bac861feda
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-01-29 14:02:22 +0900

    initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+
+      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dryopdown-item active>Active action</b-dryopdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </b-modal>
+  </div>
+</template>
