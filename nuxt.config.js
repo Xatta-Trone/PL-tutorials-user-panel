97dc45943bc8b9c71b0d980f1030490e8be02872
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-09-20 16:11:20 +0900

    new update

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,6 +115,7 @@ export default {
     {
       src: "~/plugins/mixin.js",
     },
+
     {
       src: "~/plugins/fingerprint.js",
     },
--- plugins/fingerprint.js
+++ plugins/fingerprint.js
@@ -1,4 +1,19 @@
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
+
+// import "https://cdnjs.cloudflare.com/ajax/libs/swfobject/2.2/swfobject.min.js";
+// import "~/assets/js/evercookie.js";
+// import "~/assets/js/test.js";
+
+// var ec = new evercookie();
+
+// // set a cookie "id" to "12345"
+// // usage: ec.set(key, value)
+// ec.set("id", "12345");
+
+// window.ec = ec;
+
+// console.log("ec id", ec.get("id"));
+
 window.fp = fpPromise;
 
 // Initialize an agent at application startup.
