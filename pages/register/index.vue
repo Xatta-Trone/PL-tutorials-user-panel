937735e1aedd60056bf92a4803d4f8e84c522377
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-11-03 05:19:29 +0900

    updated site

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -29,7 +29,7 @@
                             <iframe
                               width="560"
                               height="315"
-                              src="https://www.youtube-nocookie.com/embed/MeC42p1wuaM"
+                              src="https://www.youtube-nocookie.com/embed/3U3P6OKBV4o"
                               title="YouTube video player"
                               frameborder="0"
                               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
